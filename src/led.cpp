8f6d8a7a7641c1994250502a0f5696c4bda0d163
Aarón Reynoza <aaron.alejandro.reynoza@gmail.com>
2025-12-22 21:11:12 -0600

    Support NeoPixel heartbeat LED

--- src/led.cpp
+++ src/led.cpp
@@ -1,5 +1,10 @@
 #include "led.h"
 
+#if defined(ARDUINO_ARCH_ESP32)
+#include <esp32-hal-neopixel.h>
+#include <soc/soc_caps.h>
+#endif
+
 #if defined(LED_PIN)
 static constexpr int LED_BUILTIN_PIN = LED_PIN;
 #elif defined(LED_BUILTIN)
@@ -16,7 +21,26 @@ static constexpr bool LED_IS_ACTIVE_LOW = false;
 static constexpr bool LED_IS_ACTIVE_LOW = false;
 #endif
 
+#if defined(PIN_NEOPIXEL)
+static constexpr bool LED_IS_NEOPIXEL = true;
+static constexpr int LED_NEOPIXEL_PIN = PIN_NEOPIXEL;
+#elif defined(SOC_GPIO_PIN_COUNT) && defined(LED_BUILTIN)
+static constexpr bool LED_IS_NEOPIXEL = (LED_BUILTIN >= SOC_GPIO_PIN_COUNT);
+static constexpr int LED_NEOPIXEL_PIN = LED_BUILTIN - SOC_GPIO_PIN_COUNT;
+#else
+static constexpr bool LED_IS_NEOPIXEL = false;
+static constexpr int LED_NEOPIXEL_PIN = -1;
+#endif
+
+static constexpr uint8_t LED_NEOPIXEL_ON_R = 0;
+static constexpr uint8_t LED_NEOPIXEL_ON_G = 0;
+static constexpr uint8_t LED_NEOPIXEL_ON_B = 32;
+
 void initLed() {
+  if (LED_IS_NEOPIXEL) {
+    neopixelWrite(LED_NEOPIXEL_PIN, 0, 0, 0);
+    return;
+  }
   pinMode(LED_BUILTIN_PIN, OUTPUT);
 }
 
@@ -26,6 +50,18 @@ void updateLed(uint32_t nowMs) {
   if (nowMs - lastLedToggleMs >= 500) {
     lastLedToggleMs = nowMs;
     ledOn = !ledOn;
+    if (LED_IS_NEOPIXEL) {
+      if (ledOn) {
+        neopixelWrite(
+            LED_NEOPIXEL_PIN,
+            LED_NEOPIXEL_ON_R,
+            LED_NEOPIXEL_ON_G,
+            LED_NEOPIXEL_ON_B);
+      } else {
+        neopixelWrite(LED_NEOPIXEL_PIN, 0, 0, 0);
+      }
+      return;
+    }
     if (LED_IS_ACTIVE_LOW) {
       digitalWrite(LED_BUILTIN_PIN, ledOn ? LOW : HIGH);
     } else {
